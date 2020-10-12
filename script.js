ea09f030f51a7ae50ee8f70041bc81216e0d078c
Grinner28 <neil.andrewartha@gmail.com>
2020-10-12 15:13:36 +0800

    Add getClasslength squares to delete grid function so it will delete all sqaures regardless of number

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ const sketchPad = document.getElementById('sketchPadContainer')
 var w = document.getElementById('width'); //Width of sketch pad that user can adjust
 var h = document.getElementById('height'); //Height of sketch pad that user can adjust
 var gridSquares = document.getElementsByClassName('squares')
-var totalSquares = w * h;
 var i;
+generateGrid(10); //grid displayed when page is opened
 
-// create function which generates grid
-//  
+//event listener on newGrid button
+//run function deleteGrid to clear current grid
+//submit width to w var on click
+//submit height to h var on click
+//convert w to integer
+//convert h to integer
+//run function to generateGrid using 
 
 
-function deleteGrid(totalSquares) { //Deletes current grid elements 
-    for (i = totalSquares +1; i > 0; i--) {
+function deleteGrid() { //Deletes current grid elements
+    for (i = (document.getElementsByClassName('square').length +1); i > 0; i--) {
         sketchPad.removeChild(sketchPad.firstChild);
 }
 }
 
-function generateGrid(totalSquares) { //function which generates grid from input w and h
-for (i = 0; i < totalSquares; i++) {
+function generateGrid() { //function which generates grid from input w and h
+for (i = 0; i < (10); i++) {
     const newSquare = document.createElement("div"); //Creats square
     newSquare.textContent = '1'; //Will delete last, using this to visuallly function
     newSquare.classList.add('square')
